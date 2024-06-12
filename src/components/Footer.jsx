cc5147cd1f9a17e3afdd0613c03dcde4ba50cfc1
Laszlo Gall <galacy16@yahoo.com>
2024-06-12 15:37:51 +0100

    Changed layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
     };
 
     return (
-        <Box py="20" mt="6" bg="gray.900">
+        <Box py="20" mt="6" bg="blackAlpha.700">
             <Container maxW={"container.xl"} px={"20"}>
                 <Flex
                     direction={{ base: "column", md: "row" }}
