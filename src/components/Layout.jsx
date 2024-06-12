5103e2bad7bc5126b23b55a8ccd03f6a55e77108
Laszlo Gall <galacy16@yahoo.com>
2024-06-12 15:35:11 +0100

    Changed layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,21 @@
+// Layout.jsx
 import Navbar from "./Navbar";
 import PropTypes from "prop-types";
 import Footer from "../components/Footer";
 
 const Layout = ({ children }) => {
     return (
-        <>
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                minHeight: "100vh",
+            }}
+        >
             <Navbar />
-            <main>{children}</main>
+            <main style={{ flex: 1 }}>{children}</main>
             <Footer />
-        </>
+        </div>
     );
 };
 
